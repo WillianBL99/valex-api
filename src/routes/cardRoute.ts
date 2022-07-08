d5c331cf272d8fca0ad825c02f3c466c59794a75
paulo uilian <uilianlbarros@gmail.com>
2022-07-08 15:44:33 -0300

    implement schema validation

--- src/routes/cardRoute.ts
+++ src/routes/cardRoute.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from "express";
 import { apiKeyHeaderValidation } from "../middlewares/cardMiddleware.js";
+import { validateSchema } from "../middlewares/validateSchemaMiddleware.js";
 import * as cardRepository from "../repositories/cardRepository.js";
+import { cardSchema } from "../schemas/cardSchema.js";
 
 const cardRoute = Router();
 
-cardRoute.get("/", apiKeyHeaderValidation, async ( _req: Request, res: Response ) => {
+cardRoute.get("/", validateSchema( cardSchema ), apiKeyHeaderValidation, async ( _req: Request, res: Response ) => {
   const cards = await cardRepository.find();
   res.send({ cards });
 });
