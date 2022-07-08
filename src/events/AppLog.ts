835c715927bceb6cc5d4d247e909687b59baabd0
paulo uilian <uilianlbarros@gmail.com>
2022-07-07 23:12:19 -0300

    implement initial folder structure

---
+++ src/events/AppLog.ts
@@ -0,0 +1,29 @@
+import chalk from "chalk";
+import ITypes from "../interfaces/index";
+
+const types: ITypes = {
+  Middleware: 'magenta',
+  Controller: 'green',
+  Repository: 'blue',
+  Server: 'yellow',
+  Service: 'cyan',
+  Error: 'red',
+}
+
+const AppLog = (
+  type:
+    | 'Middleware'
+    | 'Controller'
+    | 'Repository'
+    | 'Server'
+    | 'Service'
+    | 'Error',
+  text: string
+) => {
+  const color = types[type] as 'green' | 'magenta' | 'blue' | 'yellow' | 'cyan' | 'red';
+  console.log(
+    chalk.bold[ color ](`[${ type }] ${ text }`)
+  );
+}
+
+export default AppLog;
