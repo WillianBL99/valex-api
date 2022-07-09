19ab1e049256eb976fa4b88e1588cc00ac5a522f
paulo uilian <uilianlbarros@gmail.com>
2022-07-09 10:02:41 -0300

    implement create card validation

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,5 +1,36 @@
+import AppError from "../config/error.js";
+import { TransactionTypes } from "../repositories/cardRepository.js";
 import * as employeeRepository from "../repositories/employeeRepository.js";
+import * as cardRepository from "../repositories/cardRepository.js";
+import { faker } from "@faker-js/faker";
 
-export async function create( cpf: number, companyId: number ) {
-  const findEmployee = await employeeRepository.findByCompanyIdAndCPF( cpf, companyId );
+export interface CreateCard {
+  cpf: number,
+  companyId: number,
+  type: TransactionTypes
+}
+
+export async function create( cardCreateData: CreateCard ) {
+  const { cpf, companyId, type } = cardCreateData;
+
+  const employee = await employeeRepository.findByCompanyIdAndCPF( cpf, companyId );
+
+  if( !employee ) {
+    throw new AppError(
+      "Employee not found",
+      404,
+      "Employee not found",
+      "Make sure this employee is employeed by this company"
+    )
+  }
+
+  const findCardSameType = await cardRepository.findByTypeAndEmployeeId( type, employee.id );
+  if( findCardSameType ) {
+    throw new AppError(
+      "there was a conflict creating this card type",
+      409,
+      "there was a conflict creating this card type",
+      "make sure this employee does not have a card of the same type"
+    )
+  }
 }
