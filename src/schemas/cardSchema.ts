8de55755a89bdfbb6acb3357d47a22cec0777027
paulo uilian <uilianlbarros@gmail.com>
2022-07-09 15:38:12 -0300

    implement card active validation

--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -4,4 +4,9 @@ const cardTypes = ["groceries", "restaurants", "transport", "education", "health
 export const cardSchema = Joi.object({
   type: Joi.string().valid(...cardTypes).required(),
   cpf: Joi.string().max(11).min(11).required()
+});
+
+export const cardActiveSchema = Joi.object({
+  cvv: Joi.string().max(3).min(3).required(),
+  password: Joi.number().max(4).min(4).required()
 })
