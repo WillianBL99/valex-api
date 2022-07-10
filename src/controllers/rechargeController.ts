20440f9f33a8412fb0263d1c037c5f765d31d1a0
paulo uilian <uilianlbarros@gmail.com>
2022-07-10 01:07:22 -0300

    validate card status

--- src/controllers/rechargeController.ts
+++ src/controllers/rechargeController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import AppError from "../config/error.js";
 import * as cardRepository from "../services/cardService.js";
+import * as rechargeRepository from "../repositories/rechargeRepository.js";
 
 export async function rechargeCard( req: Request, res: Response ) {
   const { companyId } = res.locals.companyData;
@@ -12,8 +13,16 @@ export async function rechargeCard( req: Request, res: Response ) {
       422,
       "Invalid card id",
       "Make sure to send a correct body request"
-    )
+    );
   }
 
   const card = await cardRepository.findCard( cardId );
+  if( card.isBlocked ) {
+    throw new AppError(
+      "Card blocked",
+      401,
+      "Card blocked",
+      "This card is blocked. Operation unauthorized."
+    );
+  }
 }
