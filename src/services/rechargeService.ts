4c46fad0415f67ac3b9bf05b960355941a7686e4
paulo uilian <uilianlbarros@gmail.com>
2022-07-10 04:17:07 -0300

    refactor recharge endpoint

--- src/services/rechargeService.ts
+++ src/services/rechargeService.ts
@@ -4,6 +4,7 @@ import * as rechargeRepository from "../repositories/rechargeRepository.js";
 import * as employeeRepository from "../repositories/employeeRepository.js";
 
 import AppError from "../config/error.js";
+import { Card } from "../repositories/cardRepository.js";
 
 export interface RechargeCard {
   cardId: number,
@@ -13,8 +14,16 @@ export interface RechargeCard {
 
 export async function recharge( rechardData: RechargeCard ) {
   const { cardId, companyId, amount } = rechardData;
-  
+
   const card = await cardService.findCard( cardId );
+  cardIsUnlocked( card );
+  employeeIsEployed( card, companyId );
+  await cardService.cardIsValid( card );
+  
+  await rechargeRepository.insert({ amount, cardId })
+}
+
+function cardIsUnlocked( card: Card ) {
   if( card.isBlocked ) {
     throw new AppError(
       "Card blocked",
@@ -23,7 +32,9 @@ export async function recharge( rechardData: RechargeCard ) {
       "This card is blocked. Operation unauthorized."
     );
   }
+}
 
+async function employeeIsEployed( card: Card, companyId: number ) {
   const employee = await employeeRepository.findById( card.employeeId );
   if( employee.companyId !== companyId ) {
     throw new AppError(
@@ -33,8 +44,4 @@ export async function recharge( rechardData: RechargeCard ) {
       "Make sure this employee is employeed by this company"
     );
   }
-
-  await cardService.cardIsValid( card );
-
-  await rechargeRepository.insert({ amount, cardId })
 }
